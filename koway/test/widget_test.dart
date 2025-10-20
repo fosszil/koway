777151a277c612e07687397f91b47a858d796cad
aaqil <kidobop@gmail.com>
2025-10-20 23:52:57 +0530

    changed the json data formating

--- koway/test/widget_test.dart
+++ koway/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:koway/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const KowayTravel());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
