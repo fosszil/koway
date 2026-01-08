0090df51555e154641dcbacaeb20d9b75d336467
aaqil <kidobop@gmail.com>
2026-01-08 23:37:00 +0530

    converted search filed to stateless and added clear button

--- koway/lib/widgets/search_field.dart
+++ koway/lib/widgets/search_field.dart
@@ -1,20 +1,42 @@
-import 'package:flutter/material.dart'; 
-import '../services/route_service.dart';
+import 'package:flutter/material.dart';
 
-class SearchField extends StatelessWidget {
+class SearchField extends StatefulWidget {
   final String label;
 
   const SearchField({super.key,required this.label});
 
   @override
+  State<SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState(){
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: _controller,
       decoration: InputDecoration(
-        labelText:label,
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        labelText: widget.label,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
         filled: true,
-        fillColor: Colors.grey[100],
-        )
-      ),
-    )
+        fillColor: Theme.of(context).scaffoldBackgroundColor,
+        suffixIcon: IconButton(onPressed: _controller.clear, icon: Icon(Icons.clear))
+      ),  
+    );
+  }
 }
